d443129ff13ab209687f34bcfc9a75c6d9682929
Doddy Matabaro <matabarododdy@gmail.com>
2026-02-14 08:45:54 +0200

    Add Credit Manager module with CRUD functionality for credit types
    - Implemented English and French language support for the Credit Manager module.
    - Created admin interface for managing credit types, including add, edit, delete, and export functionalities.
    - Added dashboard and statistics placeholders for the Credit Manager home page.
    - Introduced a library for handling admin page headers and tabs.
    - Ensured access control for admin functionalities based on user permissions.

---
+++ htdocs/custom/creditmanager/index.php
@@ -0,0 +1,87 @@
+<?php
+/* Copyright (C) 2026  Credit Manager module for Dolibarr
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/**
+ * \file       htdocs/custom/creditmanager/index.php
+ * \ingroup    creditmanager
+ * \brief      Dashboard / home page for Credit Manager module
+ */
+
+// Load Dolibarr environment
+$res = 0;
+if (!$res && file_exists("../main.inc.php")) {
+	$res = @include "../main.inc.php";
+}
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include "../../../main.inc.php";
+}
+if (!$res) {
+	die("Include of main fails");
+}
+
+/** @var DoliDB $db */
+/** @var Conf $conf */
+/** @var Translate $langs */
+/** @var User $user */
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
+
+$langs->loadLangs(array("creditmanager@creditmanager"));
+
+// Access control
+if (!$user->hasRight('creditmanager', 'read')) {
+	accessforbidden();
+}
+
+$action = GETPOST('action', 'aZ09');
+
+
+/*
+ * View
+ */
+
+llxHeader("", $langs->trans("CreditManager"), '', '', 0, 0, '', '', '', 'mod-creditmanager page-index');
+
+print load_fiche_titre($langs->trans("CreditManager"), '', 'object_credit@creditmanager');
+
+print '<div class="fichecenter">';
+print '<div class="fichethirdleft">';
+
+// Left column - summary boxes will go here
+print '<div class="div-table-responsive-no-min">';
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre"><th colspan="2">'.$langs->trans("CreditManagerDashboard").'</th></tr>';
+print '<tr class="oddeven"><td colspan="2" class="opacitymedium">';
+print $langs->trans("CreditManagerDashboardDescription");
+print '</td></tr>';
+print '</table>';
+print '</div>';
+
+print '</div>';
+print '<div class="fichetwothirdright">';
+
+// Right column - stats / graphs will go here
+print '<div class="div-table-responsive-no-min">';
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre"><th>'.$langs->trans("Statistics").'</th></tr>';
+print '<tr class="oddeven"><td class="opacitymedium">';
+print $langs->trans("CreditManagerStatsPlaceholder");
+print '</td></tr>';
+print '</table>';
+print '</div>';
+
+print '</div>';
+print '</div>';
+
+// End of page
+llxFooter();
+$db->close();
---
+++ htdocs/custom/creditmanager/lib/creditmanager.lib.php
@@ -0,0 +1,57 @@
+<?php
+/* Copyright (C) 2026  Credit Manager module for Dolibarr
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	\file       htdocs/custom/creditmanager/lib/creditmanager.lib.php
+ *	\ingroup    creditmanager
+ *	\brief      Library of functions for module Credit Manager
+ */
+
+/**
+ *  Prepare admin pages header (tabs)
+ *
+ *  @return	array		Array of tabs
+ */
+function creditmanagerAdminPrepareHead()
+{
+	global $langs, $conf;
+
+	$langs->load("creditmanager@creditmanager");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = dol_buildpath("/custom/creditmanager/admin/setup.php", 1);
+	$head[$h][1] = $langs->trans("Settings");
+	$head[$h][2] = 'settings';
+	$h++;
+
+	$head[$h][0] = dol_buildpath("/custom/creditmanager/admin/credit_types.php", 1);
+	$head[$h][1] = $langs->trans("CreditTypes");
+	$head[$h][2] = 'credit_types';
+	$h++;
+
+	$head[$h][0] = dol_buildpath("/custom/creditmanager/admin/attribution.php", 1);
+	$head[$h][1] = $langs->trans("CreditAttribution");
+	$head[$h][2] = 'attribution';
+	$h++;
+
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'creditmanager');
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'creditmanager', 'remove');
+
+	return $head;
+}
